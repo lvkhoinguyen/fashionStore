df3231e610155d73fdfc75f2783d730790702e69
levokhoinguyen.it@gmail.com <levokhoinguyen.it@gmail.com>
2025-12-03 21:59:23 +0700

    security login admin and user

--- src/main/java/com/clothingstore/fashionStore/config/SecurityConfiguration.java
+++ src/main/java/com/clothingstore/fashionStore/config/SecurityConfiguration.java
@@ -11,6 +11,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.HttpMessageConverterAuthenticationSuccessHandler.AuthenticationSuccess;
 
 import com.clothingstore.fashionStore.service.UserService;
 import com.clothingstore.fashionStore.service.validator.CustomUserDetailsService;
@@ -21,77 +23,89 @@ import jakarta.servlet.DispatcherType;
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfiguration {
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
 
-    // @Bean
-    // public SecurityFilterChain securityFilterChain(HttpSecurity http) throws
-    // Exception {
-    // http
-    // .authorizeHttpRequests(auth -> auth
-    // .requestMatchers("/**").permitAll()
-    // .anyRequest().authenticated())
-    // .csrf(csrf -> csrf.disable())
-    // .formLogin(form -> form.disable())
-    // .httpBasic(basic -> basic.disable());
+        // @Bean
+        // public SecurityFilterChain securityFilterChain(HttpSecurity http) throws
+        // Exception {
+        // http
+        // .authorizeHttpRequests(auth -> auth
+        // .requestMatchers("/**").permitAll()
+        // .anyRequest().authenticated())
+        // .csrf(csrf -> csrf.disable())
+        // .formLogin(form -> form.disable())
+        // .httpBasic(basic -> basic.disable());
 
-    // return http.build();
-    // }
+        // return http.build();
+        // }
 
-    @Bean
-    public UserDetailsService userDetailsService(UserService userService) {
-        return new CustomUserDetailsService(userService);
+        @Bean
+        public UserDetailsService userDetailsService(UserService userService) {
+                return new CustomUserDetailsService(userService);
 
-    }
+        }
 
-    @Bean
-    public AuthenticationManager authenticationManager(HttpSecurity http,
-            PasswordEncoder passwordEncoder,
-            UserDetailsService userDetailsService) throws Exception {
-        AuthenticationManagerBuilder authenticationManagerBuilder = http
-                .getSharedObject(AuthenticationManagerBuilder.class);
-        authenticationManagerBuilder
-                .userDetailsService(userDetailsService)
-                .passwordEncoder(passwordEncoder);
-        return authenticationManagerBuilder.build();
-    }
+        @Bean
+        public AuthenticationManager authenticationManager(HttpSecurity http,
+                        PasswordEncoder passwordEncoder,
+                        UserDetailsService userDetailsService) throws Exception {
+                AuthenticationManagerBuilder authenticationManagerBuilder = http
+                                .getSharedObject(AuthenticationManagerBuilder.class);
+                authenticationManagerBuilder
+                                .userDetailsService(userDetailsService)
+                                .passwordEncoder(passwordEncoder);
+                return authenticationManagerBuilder.build();
+        }
 
-    @Bean
-    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        @Bean
+        public AuthenticationSuccessHandler customSuccessHandler() {
+                return new CustomSuccesHandler();
+        }
 
-        http
-                .csrf(csrf -> csrf.disable())
+        @Bean
+        SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-                .authorizeHttpRequests(auth -> auth
-                        .dispatcherTypeMatchers(DispatcherType.FORWARD,
-                                DispatcherType.INCLUDE)
-                        .permitAll()
-                        .requestMatchers("/", "/login", "/css/**", "/js/**", "/img/**", "/assets/**",
-                                "/client/**")
-                        .permitAll()
-                        .anyRequest().authenticated())
+                http
+                                .csrf(csrf -> csrf.disable())
 
-                .formLogin(login -> login
-                        .loginPage("/login")
-                        .failureUrl("/login?error")
-                        .permitAll());
+                                .authorizeHttpRequests(auth -> auth
+                                                .dispatcherTypeMatchers(DispatcherType.FORWARD,
+                                                                DispatcherType.INCLUDE)
+                                                .permitAll()
+                                                .requestMatchers("/", "/login", "/product/detail/**", "/css/**",
+                                                                "/js/**",
+                                                                "/img/**", "/assets/**",
+                                                                "/client/**")
+                                                .permitAll()
 
-        return http.build();
-    }
+                                                .requestMatchers("/admin/**").hasRole("ADMIN")
+                                                .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
 
-    @Bean
-    public DaoAuthenticationProvider authProvider(
-            PasswordEncoder passwordEncoder,
-            UserDetailsService userDetailsService) {
+                                                .anyRequest().authenticated())
 
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService);
-        authProvider.setPasswordEncoder(passwordEncoder);
-        authProvider.setHideUserNotFoundExceptions(false);
+                                .formLogin(login -> login
+                                                .loginPage("/login")
+                                                .failureUrl("/login?error")
+                                                .successHandler(customSuccessHandler())
+                                                .permitAll());
 
-        return authProvider;
-    }
+                return http.build();
+        }
+
+        @Bean
+        public DaoAuthenticationProvider authProvider(
+                        PasswordEncoder passwordEncoder,
+                        UserDetailsService userDetailsService) {
+
+                DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+                authProvider.setUserDetailsService(userDetailsService);
+                authProvider.setPasswordEncoder(passwordEncoder);
+                authProvider.setHideUserNotFoundExceptions(false);
+
+                return authProvider;
+        }
 
 }
